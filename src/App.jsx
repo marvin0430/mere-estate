875d95def88c1efa2e88187adb27c3143fc8ccd2
Marvin <marvinlaton06@gmail.com>
2024-05-26 14:44:59 +0800

    Added React Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
 
 function App() {
-   return <h1>App</h1>
+    return (
+        <BrowserRouter>
+            <Routes path={'/'} element={<Home />} />
+            <Routes path={'/sign-in'} element={<SignIn />} />
+            <Routes path={'/sign-out'} element={<SignUp />} />
+            <Routes path={'/about'} element={<About />} />
+            <Routes path={'/profile'} element={<Profile />} />
+        </BrowserRouter>
+    );
 }
 
 export default App;
